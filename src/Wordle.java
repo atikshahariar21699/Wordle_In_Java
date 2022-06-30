c84fcc807fd0ee114b85b9ce7e16cc93aadbd1a5
atikshahariar21699 <55033560+atikshahariar21699@users.noreply.github.com>
2022-07-01 01:44:05 +0600

    2nd commit

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,16 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 public class Wordle {
 
-    public void word_list(){
+     private static List word_list() throws FileNotFoundException {
+        File file = new File("E:\\Wordle In Java\\Wordle_in_java\\src\\test.txt");
+        Scanner input = new Scanner(file);
+        List<String> wordlist=new ArrayList<String>();
+        int count = 0;
+        while (input.hasNext()) {
+            String word  = input.next();
+            wordlist.add(word);
+           // System.out.println(word);
+           // count = count + 1;
+        }
+        //System.out.println(wordlist.size());
+        return wordlist;
 
+        //System.out.println("Word count: " + count);
     }
 
+    private static String random_word(List<String> wordlist){
+        Random r=new Random();
+        int random_index=r.nextInt(wordlist.size());
+        return wordlist.get(random_index);
+    }
 
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    public static void main(String[] args) throws FileNotFoundException {
+       /* Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter username ");
 
         String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);
+        System.out.println("Username is: " + userName);*/
+        List<String> wordlist=word_list();
+
+        System.out.println(random_word(wordlist));
     }
 }
